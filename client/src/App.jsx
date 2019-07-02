14668ad8dd54d19c17e1c1d5739c691534b9e839
Ariel <a.o.hyman@gmail.com>
2019-07-01 23:04:33 -0400

    Initial Env Config
    Co-authored-by: Ariel Hyman <a.o.hyman@gmail.com>
    Co-authored-by: Joel Rivera <realjoelrivera@gmail.com>

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+// Imports: Dependencies
+import React, { Component } from 'react';
+
+// Imports: Components
+
+// React Application
+const App = () => {
+
+    return (
+      <div id="main">
+          rendering app gobbeldygook
+      </div>
+    );
+}
+
+// Exports
+export default App;
---
+++ client/src/index.jsx
@@ -0,0 +1,11 @@
+// Imports: React
+import React from 'react';
+import {render} from 'react-dom';
+import App from './App.jsx'
+// Imports: CSS
+import './styles.css'; 
+
+
+
+// Render App
+render(<App />, document.getElementById('app'));
