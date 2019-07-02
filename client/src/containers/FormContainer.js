dac010addb781e866a2d5c30b355d06cc0028236
Joel <realjoelrivera@gmail.com>
2019-07-02 14:26:12 -0400

    Added FormContainer to app

---
+++ client/src/containers/FormContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FormComponent from '../components/FormComponent';
+
+const FormContainer = () => {
+
+  return (
+    <div>
+      <h4>Form Container rendering:</h4>
+      <FormComponent />
+    </div>
+  );
+
+};
+
+export default FormContainer;
