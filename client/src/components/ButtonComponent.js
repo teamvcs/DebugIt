ab0d52b6b39d168ecd343d309c9246bdb07fe5f9
Joel <realjoelrivera@gmail.com>
2019-07-02 15:25:54 -0400

    The ButtonComponent. To be rendered by different components.

---
+++ client/src/components/ButtonComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const ButtonComponent = () => {
+
+  return (
+    <div>
+      ButtonComponent
+    </div>
+  );
+
+};
+
+export default ButtonComponent;
