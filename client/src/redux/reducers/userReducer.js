89eebf6c9ead7e39b08159d3d8b6a032f10f3b05
Joel <realjoelrivera@gmail.com>
2019-07-03 07:58:33 -0400

    Implemented the template for the ADD_USER action for the userReducer to switch on

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -1,13 +1,18 @@
+// import { ADD_USER } from '../actions/actions';
+
 const initalState = {
   isLoggedIn: false,
   questionAnswered: false,
   questionAnsweredCorrectly: false,
   totalAnswered: [],
-
+  // user: { name: 'Joel' },
 };
 
 const userReducer = (state = initalState, action) => {
   switch (action.type) {
+    // case ADD_USER:
+    //   console.log(state, action);
+    //   return { ...state, user: action.payload };
     default:
       return state;
   }
