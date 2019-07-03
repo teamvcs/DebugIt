3ac9d9c8b34538f3640a91e9e98e93b3926dbf4d
Alena Budzko <budzko.alena@gmail.com>
2019-07-03 10:06:52 -0400

    Completed functionality for server signup and login routes (#6)
    * Changed .gitignore; basic functionality for server.js
    * Added middleware functionality for confirm user

--- server/server.js
+++ server/server.js
@@ -1,31 +1,46 @@
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// Imports: Dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const path = require('path');
-const app = express();
-const addUser = require('./controllers/dbControlller');
+const cors = require('cors');
+const PORT = 3000;
 
+const { addUser } = require('./controllers/userController');
+const { bCryptPassword, verifyPassword } = require('./controllers/authController');
+
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors());
 
-app.get('/', (req,res)=>{
-  res.set({'Content-Type': 'text/html'})
-  .status(200)
-  .sendFile(path.join(__dirname, './../client/dist/index.html'))
+// Serve Static Files
+app.use(express.static(path.join(__dirname, '../client/public')));
+
+// initial load
+app.get('/', (req, res, next) => {
+  res.set('Content-Type', 'index/html')
+    .status(200)
+    .sendFile(path.join(__dirname, '../client/dist/index.html'));
 })
 
-app.post('/', addUser, (req,res)=>{
-  
+app.get('/login', verifyPassword, (req, res, next) => {
+  console.log("I am passed!")
+  // res.json(res.locals.user);
 })
 
-const PORT = 4000 || process.env;
+app.post('/signup', bCryptPassword, addUser, (req, res, next) => {
+  res.json(res.locals.user);
+})
 
+app.get('/problem:ID', (req, res, next) => {
+  // do stuff
+})
 
 app.listen(PORT, () => {
-  console.log(`The server has started on port: ${PORT}`);
-  console.log(`http://localhost:${PORT}`);
+  console.log(`The server has been started at http://localhost:${PORT}`);
 });
 
 module.exports = app;
