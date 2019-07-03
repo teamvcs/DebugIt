a4dc931a47c841120e07ecf40428bf45bda37ec4
Jake Douglas <jbrandondouglas@gmail.com>
2019-07-03 11:43:29 -0400

    google oauth attempts

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -54,38 +54,21 @@ authController.verifyPassword = (req, res, next) => {
     axios
       .get(url)
       .then(response => {
+        console.log('response: ', response);
         res.send(response.data);
       })
       .catch(error => {
+
         console.log('Google Oauth getting code:', error);
       });
   },
 
-  authController.OAuthGetToken = (req, res, next) => {
-    console.log('in OAuthGetToken');
+  authController.OAuthGetToken = async (req, res, next) => {
+    // console.log('in OAuthGetToken');
     const { code } = req.query;
     // const sessionState = req.query.session_state;
-    // console.log('req :', req);
-    axios
-      .post(url)
-      .then(response => {
-        // console.log('response!!!!!: ', response);
-        console.log('response.data!!!!!!: ', response.data);
-        // // Oauth slides: 5. GitHub responds to Server with Access Token
-        // let jwt = response.data.id_token;
-        // jwt = jwt.split('.')[1];
-        // const base64 = Buffer.from(jwt, 'base64').toString();
-        // const { email } = JSON.parse(base64);
-        // res.locals.email = email;
-        // // res.cookie('email', email);
-
-        // // Oauth slides: 5. Server saves Token in Cookie
-        // res.cookie('jwt', jwt, { expires: new Date(Date.now() + 900000) });
-        //   return next();
-        // })
-        // .catch(err => {
-        //   console.log('Error in getting OAuth token', err);
-      })
+    const { tokens } = await oauth2Client.getToken(code);
+    oauth2Client.setCredentials(tokens);
   }
 
 module.exports = authController 
