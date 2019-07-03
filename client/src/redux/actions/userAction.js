08bf26e18d392a48db12ae4d6a18ddc53696396b
Joel <realjoelrivera@gmail.com>
2019-07-03 07:57:15 -0400

    Added the boilerplate for one action in the application

---
+++ client/src/redux/actions/userAction.js
@@ -0,0 +1,7 @@
+// import { ADD_USER } from './actions';
+
+// export const userSignUp = (user) => {
+//   user.name = 'Ariel';
+//   console.log(user);
+//   return { type: ADD_USER, payload: user };
+// };
